4c1360e350da933fe3c7d2fe8103a5b064aab0f5
Houcem Hedhly <hedhoucem@gmail.com>
2021-09-03 14:31:07 +0100

    export vanbar to component based on bootstrap

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,3 @@
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
-  "rules": {
-    "prettier/prettier": ["error", { "singleQuote": true }]
-  },
 };
---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Tenth navbar example">
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample08" aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample08">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+              <router-link to="/" class="nav-link">Home</router-link>
+          </li>
+          <li class="nav-item">
+              <router-link to="/about" class="nav-link">About</router-link>
+          </li>
+          <li class="nav-item">
+              <router-link to="/contact" class="nav-link">Contact</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
